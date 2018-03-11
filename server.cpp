7a6aebb8ea99b1f5fba43da8bd2c6905424dc002
Aurorayqz <aurorayqz@foxmail.com>
2018-03-11 22:18:06 +0800

    Modify the style

--- server.cpp
+++ server.cpp
@@ -1,105 +1,96 @@
 #include "utility.h"
 
-int main(int argc, char const *argv[])
+int main( int argc, char const *argv[] )
 {
 	struct sockaddr_in serverAddr;
-	serverAddr.sin_family = PF_INET;
-	serverAddr.sin_port=htons(SERVER_PORT);
-	serverAddr.sin_addr.s_addr=inet_addr(SERVER_IP);
+	serverAddr.sin_family		= PF_INET;
+	serverAddr.sin_port			= htons( SERVER_PORT );
+	serverAddr.sin_addr.s_addr	= inet_addr( SERVER_IP );
 
-	int listener=socket(PF_INET,SOCK_STREAM,0);
-	if (listener<0)
+	int listener = socket( PF_INET, SOCK_STREAM, 0 );
+	if ( listener < 0 )
 	{
-		perror("listener");
-		exit(-1);
+		perror( "listener" );
+		exit( -1 );
 	}
-	printf("listen socket created \n");
+	printf( "listen socket created \n" );
 
-	if (bind(listener, (struct sockaddr *)&serverAddr, sizeof(serverAddr))<0)
+	if ( bind( listener, (struct sockaddr *) &serverAddr, sizeof(serverAddr) ) < 0 )
 	{
-		perror("bind error");
-		exit(-1);
+		perror( "bind error" );
+		exit( -1 );
 	}
 
-	int ret = listen(listener, 5);
-	if (ret<0)
+	int ret = listen( listener, 5 );
+	if ( ret < 0 )
 	{
-		perror("listen error");
-		exit(-1);
+		perror( "listen error" );
+		exit( -1 );
 	}
-	printf("Start to listen: %s\n", SERVER_IP);
+	printf( "Start to listen: %s\n", SERVER_IP );
 
-	//在内核中创建事件表
-	int epfd = epoll_create(EPOLL_SIZE);
-	if (epfd<0)
+	int epfd = epoll_create( EPOLL_SIZE );
+	if ( epfd < 0 )
 	{
-		perror("epfd error");
-		exit(-1);
+		perror( "epfd error" );
+		exit( -1 );
 	}
-	printf("epoll created, epollfd = %d\n", epfd);
+	printf( "epoll created, epollfd = %d\n", epfd );
 	static struct epoll_event events[EPOLL_SIZE];
-	//往内核事件表里添加事件
-	addfd(epfd,listener,true);
 
-	while(1)
+	addfd( epfd, listener, true );
+
+	while ( 1 )
 	{
-		//epoll_events_count表示就绪事件的数目
-		int epoll_events_count=epoll_wait(epfd,events,EPOLL_SIZE,-1);
-		if (epoll_events_count<0)
+		int epoll_events_count = epoll_wait( epfd, events, EPOLL_SIZE, -1 );
+		if ( epoll_events_count < 0 )
 		{
-			perror("epoll failure");
+			perror( "epoll failure" );
 			break;
 		}
-		printf("epoll_events_count = %d\n", epoll_events_count);
+		printf( "epoll_events_count = %d\n", epoll_events_count );
 
-		//处理这epoll_events_count个就绪事件
-		for (int i = 0; i < epoll_events_count; ++i)
+		for ( int i = 0; i < epoll_events_count; ++i )
 		{
 			int sockfd = events[i].data.fd;
 
-			if (sockfd==listener)
+			if ( sockfd == listener )
 			{
-				struct sockaddr_in client_address;
-				socklen_t client_addrLength = sizeof(struct sockaddr_in);
-				int clientfd=accept(listener,(struct sockaddr*)&client_address,&client_addrLength);
-				printf("client connection from: %s : % d(IP : port), clientfd = %d \n",
-					inet_ntoa(client_address.sin_addr),
-					ntohs(client_address.sin_port),
+				struct sockaddr_in	client_address;
+				socklen_t		client_addrLength	= sizeof(struct sockaddr_in);
+				int			clientfd		= accept( listener, (struct sockaddr *) &client_address, &client_addrLength );
+				printf( "client connection from: %s : % d(IP : port), clientfd = %d \n",
+					inet_ntoa( client_address.sin_addr ),
+					ntohs( client_address.sin_port ),
 					clientfd
 					);
-				addfd(epfd, clientfd, true);
+				addfd( epfd, clientfd, true );
 
-				// 服务端用list保存用户连接
-				clients_list.push_back(clientfd);
-				printf("Add new clientfd = %d to epoll\n", clientfd);
-				printf("Now there are %d clients int the chat room\n", (int)clients_list.size());
+				clients_list.push_back( clientfd );
+				printf( "Add new clientfd = %d to epoll\n", clientfd );
+				printf( "Now there are %d clients int the chat room\n", (int) clients_list.size() );
+				printf( "welcome message\n" );
 
-				// 服务端发送欢迎信息  
-                printf("welcome message\n");  
-
-                char message[BUF_SIZE];
-                bzero(message, BUF_SIZE);
-                sprintf(message, SERVER_WELCOME,clientfd);
-                int ret=send(clientfd,message,BUF_SIZE,0);
-                if (ret<0)
-                {
-                	perror("send error");
-                	exit(-1);
-                }
-                else
-                {
-                	int ret=sendBroadcastmessage(sockfd);
-                	if (ret<0)
-                	{
-                		perror("error");
-                		exit(-1);
-                	}
-                }
+				char message[BUF_SIZE];
+				bzero( message, BUF_SIZE );
+				sprintf( message, SERVER_WELCOME, clientfd );
+				int ret = send( clientfd, message, BUF_SIZE, 0 );
+				if ( ret < 0 )
+				{
+					perror( "send error" );
+					exit( -1 );
+				}else  {
+					int ret = sendBroadcastmessage( sockfd );
+					if ( ret < 0 )
+					{
+						perror( "error" );
+						exit( -1 );
+					}
+				}
 			}
 		}
 	}
-
-	close(listener);
-	close(epfd);
-	return 0;
+	close( listener );
+	close( epfd );
+	return(0);
 }
--- utility.h
+++ utility.h
@@ -35,70 +35,63 @@ list<int> clients_list;
 
 #define CAUTION "There is only one int the char room!"
 
-//设置非阻塞
-int setnonblocking(int sockfd)
+int setnonblocking( int sockfd )
 {
-	fcntl(sockfd, F_SETFL,fcntl(sockfd,F_GETFD,0)|O_NONBLOCK);
-	return 0;
+	fcntl( sockfd, F_SETFL, fcntl( sockfd, F_GETFD, 0 ) | O_NONBLOCK );
+	return(0);
 }
 
-//将文件描述符fd添加到epollfd标示的内核事件表
 void addfd( int epollfd, int fd, bool enable_et )
 {
 	struct epoll_event ev;
-	ev.data.fd = fd;
-	ev.events = EPOLLIN;
-	if (enable_et)
+	ev.data.fd	= fd;
+	ev.events	= EPOLLIN;
+	if ( enable_et )
 	{
 		ev.events = EPOLLIN | EPOLLET;
 	}
-	epoll_ctl(epollfd, EPOLL_CTL_ADD, fd, &ev);
-	setnonblocking(fd);
-	printf("fd added to epoll!\n\n");
+	epoll_ctl( epollfd, EPOLL_CTL_ADD, fd, &ev );
+	setnonblocking( fd );
+	printf( "fd added to epoll!\n\n" );
 }
 
-//服务端发送广播信息，使所有用户都能收到消息
-int sendBroadcastmessage(int clientfd)
+int sendBroadcastmessage( int clientfd )
 {
-	char buf[BUF_SIZE],message[BUF_SIZE];
-	bzero(buf,BUF_SIZE);
-	bzero(message,BUF_SIZE);
+	char buf[BUF_SIZE], message[BUF_SIZE];
+	bzero( buf, BUF_SIZE );
+	bzero( message, BUF_SIZE );
 
-	printf("read from client(clientID = %d)\n", clientfd);
-	int len=recv(clientfd,buf,BUF_SIZE,0);
+	printf( "read from client(clientID = %d)\n", clientfd );
+	int len = recv( clientfd, buf, BUF_SIZE, 0 );
 
-	if (len == 0)
+	if ( len == 0 )
 	{
-		close(clientfd);
-		clients_list.remove(clientfd);
-		printf("ClientID = %d closed.\n now there are %d client in the char room\n", clientfd, (int)clients_list.size());
-	}
-	else
-	{
-		if (clients_list.size()==1)
+		close( clientfd );
+		clients_list.remove( clientfd );
+		printf( "ClientID = %d closed.\n now there are %d client in the char room\n", clientfd, (int) clients_list.size() );
+	}else  {
+		if ( clients_list.size() == 1 )
 		{
-			send(clientfd, CAUTION, strlen(CAUTION), 0);
-			return len;
+			send( clientfd, CAUTION, strlen( CAUTION ), 0 );
+			return(len);
 		}
 
-		sprintf(message, SERVER_MESSAGE,clientfd,buf);
+		sprintf( message, SERVER_MESSAGE, clientfd, buf );
 
 		list<int>::iterator it;
-		for (it = clients_list.begin();it!=clients_list.end();++it)
+		for ( it = clients_list.begin(); it != clients_list.end(); ++it )
 		{
-			if (*it!=clientfd)
+			if ( *it != clientfd )
 			{
-				if (send(*it, message, BUF_SIZE, 0)<0)
+				if ( send( *it, message, BUF_SIZE, 0 ) < 0 )
 				{
-					perror("error");
-					exit(-1);
+					perror( "error" );
+					exit( -1 );
 				}
 			}
 		}
-
 	}
-	return len;
+	return(len);
 }
 
-
-#endif // UTILITY_H_INCLUDED
+#endif /* UTILITY_H_INCLUDED */
